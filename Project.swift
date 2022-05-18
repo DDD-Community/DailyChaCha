88e9e0886fcfa8f2dc0fa4677f13db84fd2d3ed7
Jaewook__ <31857308+woookDev@users.noreply.github.com>
2022-05-18 23:02:23 +0900

    feat: Tuist setup & base config

---
+++ Project.swift
@@ -0,0 +1,54 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+// MARK: - Project Factory
+
+protocol ProjectFactory {
+    var projectName: String { get }
+    var dependencies: [TargetDependency] { get }
+
+    func generate() -> [Target]
+}
+
+// MARK: - Base Project Factory
+
+class BaseProjectFactory: ProjectFactory {
+    let projectName: String = "DailyChaCha"
+
+    let dependencies: [TargetDependency] = []
+
+    func generate() -> [Target] {
+        [
+            Target(
+                name: projectName,
+                platform: .iOS,
+                product: .app,
+                bundleId: "com.dailychacha.\(projectName)",
+                infoPlist: .default,
+                sources: ["\(projectName)/\(projectName)/Sources/**"],
+                resources: ["\(projectName)/\(projectName)/Resources/**"],
+                dependencies: dependencies
+            ),
+
+            Target(
+                name: "\(projectName)Tests",
+                platform: .iOS,
+                product: .unitTests,
+                bundleId: "com.dailychacha.\(projectName).Tests",
+                infoPlist: .default,
+                sources: ["\(projectName)/\(projectName)Tests/**"],
+                dependencies: [.target(name: "DailyChaCha")]
+            )
+        ]
+    }
+}
+
+// MARK: - Project
+
+let factory = BaseProjectFactory()
+
+let project: Project = .init(
+    name: factory.projectName,
+    organizationName: factory.projectName,
+    targets: factory.generate()
+)
---
+++ Tuist/Dependencies.swift
@@ -0,0 +1,15 @@
+import ProjectDescription
+
+let dependencies = Dependencies(
+    swiftPackageManager: .init(
+        [
+            .remote(url: "https://github.com/uber/RIBs.git", requirement: .upToNextMinor(from: "v0.13.0")),
+            .remote(url: "https://github.com/Moya/Moya.git", requirement: .upToNextMinor(from: "15.0.0")),
+            .remote(url: "https://github.com/SnapKit/SnapKit.git", requirement: .upToNextMinor(from: "5.0.0")),
+            .remote(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", requirement: .upToNextMinor(from: "1.9.0")),
+            .remote(url: "https://github.com/ReactiveX/RxSwift.git", requirement: .upToNextMinor(from: "6.5.0")),
+            .remote(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", requirement: .upToNextMinor(from: "5.0.0")),
+        ],
+    ),
+    platforms: [.iOS]
+)
