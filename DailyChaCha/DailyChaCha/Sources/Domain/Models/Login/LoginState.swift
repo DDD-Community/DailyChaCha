cbb658b4e5713d9c80ad1405f1a30836a30f0b11
woookdev <hjw0911@gmail.com>
2022-06-16 22:40:54 +0900

    feat: LoginState 추가

--- DailyChaCha/DailyChaCha/Sources/Domain/Models/Login/LoginState.swift
+++ DailyChaCha/DailyChaCha/Sources/Domain/Models/Login/LoginState.swift
@@ -1,9 +1,13 @@
 //
-//  EmptyModel.swift
+//  LoginState.swift
 //  DailyChaCha
 //
-//  Created by Jaewook Hwang on 2022/05/23.
+//  Created by Jaewook Hwang on 2022/06/16.
 //  Copyright © 2022 DailyChaCha. All rights reserved.
 //
 
 import Foundation
+
+struct LoginState {
+  
+}
