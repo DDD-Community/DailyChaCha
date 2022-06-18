93741452ad5b20ed09e2c50b5c1ba3f5ddb8dbfe
woookdev <hjw0911@gmail.com>
2022-06-18 22:04:15 +0900

    chore: indent 수정

--- DailyChaCha/DailyChaCha/Sources/Application/Extensions/DelegateProxies/ASAuthorizationControllerProxy.swift
+++ DailyChaCha/DailyChaCha/Sources/Application/Extensions/DelegateProxies/ASAuthorizationControllerProxy.swift
@@ -12,41 +12,46 @@ import RxSwift
 import UIKit
 
 extension ASAuthorizationController: HasDelegate {
-    public typealias Delegate = ASAuthorizationControllerDelegate
+  public typealias Delegate = ASAuthorizationControllerDelegate
 }
 
-class ASAuthorizationControllerProxy: DelegateProxy<ASAuthorizationController, ASAuthorizationControllerDelegate>, DelegateProxyType, ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
-    var presentationWindow: UIWindow = UIWindow()
-
-    public init(controller: ASAuthorizationController) {
-        super.init(parentObject: controller, delegateProxy: ASAuthorizationControllerProxy.self)
-    }
-
-    // MARK: - DelegateProxyType
-    public static func registerKnownImplementations() {
-        register { ASAuthorizationControllerProxy(controller: $0) }
-    }
-
-    // MARK: - Proxy Subject
-    internal lazy var didComplete = PublishSubject<ASAuthorization>()
-
-    // MARK: - ASAuthorizationControllerDelegate
-    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
-        didComplete.onNext(authorization)
-        didComplete.onCompleted()
-    }
-
-    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
-        didComplete.onCompleted()
-    }
-
-    // MARK: - ASAuthorizationControllerPresentationContextProviding
-    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
-        return presentationWindow
-    }
-
-    // MARK: - Completed
-    deinit {
-        self.didComplete.onCompleted()
-    }
+class ASAuthorizationControllerProxy:
+  DelegateProxy<ASAuthorizationController, ASAuthorizationControllerDelegate>,
+  DelegateProxyType,
+  ASAuthorizationControllerDelegate,
+  ASAuthorizationControllerPresentationContextProviding
+{
+  var presentationWindow: UIWindow = UIWindow()
+  
+  public init(controller: ASAuthorizationController) {
+    super.init(parentObject: controller, delegateProxy: ASAuthorizationControllerProxy.self)
+  }
+  
+  // MARK: - DelegateProxyType
+  public static func registerKnownImplementations() {
+    register { ASAuthorizationControllerProxy(controller: $0) }
+  }
+  
+  // MARK: - Proxy Subject
+  internal lazy var didComplete = PublishSubject<ASAuthorization>()
+  
+  // MARK: - ASAuthorizationControllerDelegate
+  func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+    didComplete.onNext(authorization)
+    didComplete.onCompleted()
+  }
+  
+  func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+    didComplete.onCompleted()
+  }
+  
+  // MARK: - ASAuthorizationControllerPresentationContextProviding
+  func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+    return presentationWindow
+  }
+  
+  // MARK: - Completed
+  deinit {
+    self.didComplete.onCompleted()
+  }
 }
