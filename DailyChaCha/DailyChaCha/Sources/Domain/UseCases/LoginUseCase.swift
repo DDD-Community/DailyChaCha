9ccf27bf21d13f569b9801b9f86fab1a356a38a2
woookdev <hjw0911@gmail.com>
2022-06-18 22:05:05 +0900

    feat: LoginUseCase 추가

---
+++ DailyChaCha/DailyChaCha/Sources/Domain/UseCases/LoginUseCase.swift
@@ -0,0 +1,16 @@
+//
+//  LoginUseCase.swift
+//  DailyChaCha
+//
+//  Created by Jaewook Hwang on 2022/06/16.
+//  Copyright © 2022 DailyChaCha. All rights reserved.
+//
+
+import Foundation
+
+protocol LoginUseCase {
+    
+}
+
+final class LoginUseCaseImpl: LoginUseCase {
+}
