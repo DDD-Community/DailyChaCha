156bd073b9945fd2ffbe3c468547d51962295590
woookdev <hjw0911@gmail.com>
2022-06-18 22:00:40 +0900

    feat: LoginInteractor boilerplate 구현

--- DailyChaCha/DailyChaCha/Sources/Presentation/Scenes/Login/LoginInteractor.swift
+++ DailyChaCha/DailyChaCha/Sources/Presentation/Scenes/Login/LoginInteractor.swift
@@ -6,4 +6,69 @@
 //  Copyright © 2022 DailyChaCha. All rights reserved.
 //
 
-import Foundation
+import RIBs
+import RxSwift
+import ReactorKit
+
+protocol LoginRouting: ViewableRouting {
+    
+}
+
+protocol LoginPresentable: Presentable {
+    var listener: LoginPresentableListener? { get set }
+}
+
+protocol LoginListener: AnyObject { }
+
+final class LoginInteractor: PresentableInteractor<LoginPresentable>,
+                             LoginInteractable,
+                             LoginPresentableListener,
+                              Reactor
+{
+    // MARK: - Constants
+
+    enum Mutation {
+        
+    }
+
+    // MARK: - Properties
+
+    var initialState: LoginState = .init()
+
+    weak var router: LoginRouting?
+    weak var listener: LoginListener?
+
+    private let useCase: LoginUseCase
+
+    // MARK: - Con(De)structor
+
+    init(
+        presenter: LoginPresentable,
+        useCase: LoginUseCase
+    ) {
+        self.useCase = useCase
+        super.init(presenter: presenter)
+        presenter.listener = self
+    }
+}
+
+// MARK: - Reactor
+extension LoginInteractor {
+
+    // MARK: - Mutate
+
+    func mutate(action: LoginAction) -> Observable<Mutation> {
+      return Observable<Mutation>.empty()
+    }
+
+    // MARK: - Mutations
+
+
+    // MARK: - Reduce
+
+    func reduce(state: LoginState, mutation: Mutation) -> LoginState {
+        var newState = state
+
+        return newState
+    }
+}
