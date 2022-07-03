42301abd9a689fb111945068446eecfab9dc8906
woookdev <hjw0911@gmail.com>
2022-07-03 20:10:16 +0900

    feat: CoachMarkTooltipView 추가

---
+++ DailyChaCha/DailyChaCha/Sources/Presentation/Scenes/HomeCoachMark/Views/CoachMarkTooltipView.swift
@@ -0,0 +1,32 @@
+//
+//  CoachMarkTooltipView.swift
+//  DailyChaCha
+//
+//  Created by Jaewook Hwang on 2022/07/03.
+//  Copyright © 2022 DailyChaCha. All rights reserved.
+//
+
+import UIKit
+
+final class CoachMarkTooltipView: UIView {
+  
+  private let stepLabel = UILabel()
+  
+  private let titleLabel = UILabel()
+  
+  private let button = UIButton()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+extension CoachMarkTooltipView {
+  
+}
+
+
