d4b3dc4afd29f0695dda8fd9b521f909f3ed18b8
woookdev <hjw0911@gmail.com>
2022-07-04 03:15:29 +0900

    feat: CoachMarkTooltipView builder 패턴 setting method 추가

--- DailyChaCha/DailyChaCha/Sources/Presentation/Scenes/HomeCoachMark/Views/CoachMarkTooltipView.swift
+++ DailyChaCha/DailyChaCha/Sources/Presentation/Scenes/HomeCoachMark/Views/CoachMarkTooltipView.swift
@@ -8,7 +8,12 @@
 
 import UIKit
 
+import RxSwift
+import RxCocoa
+
 final class CoachMarkTooltipView: UIView {
+  
+  // MARK: - Constants
   
   enum BubbleArrowPosition {
     case topLeft
@@ -19,13 +24,24 @@ final class CoachMarkTooltipView: UIView {
     case bottomRight
   }
   
-  private let stepLabel = UILabel()
+  // MARK: - UI Components
+  
+  private let subTitleLabel = UILabel()
   
   private let titleLabel = UILabel()
   
-  private let button = UIButton()
+  private let button = UIButton().then {
+    $0.backgroundColor = UIColor.orange
+    $0.setTitle("다음", for: .normal)
+  }
   
-  private let arrowImageView = UIImageView()
+  private let arrowImageView = UIImageView().then {
+    $0.contentMode = .scaleAspectFit
+  }
+  
+  // MARK: - Properties
+  
+  lazy var buttonTapped = button.rx.tap
   
   init(
     frame: CGRect,
@@ -33,18 +49,82 @@ final class CoachMarkTooltipView: UIView {
   ) {
     super.init(frame: frame)
     setupUI()
+    setBubbleArrow(position: arrowPosition)
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
   
-  // MARK: - Private Methods
+  // MARK: - Internal Methods
   
-  func setBubbleArrow(position: BubbleArrowPosition) {
+  @discardableResult
+  func setBubbleArrow(position: BubbleArrowPosition) -> CoachMarkTooltipView {
     arrowImageView.snp.removeConstraints()
     
+    switch position {
+    case .bottomLeft:
+      arrowImageView.image = UIImage(named: "tooltip_arrow_down")
+      arrowImageView.snp.makeConstraints {
+        $0.top.equalTo(self.snp.bottom)
+        $0.leading.equalToSuperview().inset(40)
+        $0.height.equalTo(13)
+        $0.width.equalTo(25)
+      }
+    case .bottomMiddle:
+      arrowImageView.image = UIImage(named: "tooltip_arrow_down")
+      arrowImageView.snp.makeConstraints {
+        $0.top.equalTo(self.snp.bottom)
+        $0.centerX.equalToSuperview()
+        $0.height.equalTo(13)
+        $0.width.equalTo(25)
+      }
+    case .bottomRight:
+      arrowImageView.image = UIImage(named: "tooltip_arrow_down")
+      arrowImageView.snp.makeConstraints {
+        $0.top.equalTo(self.snp.bottom)
+        $0.trailing.equalToSuperview().inset(40)
+        $0.height.equalTo(13)
+        $0.width.equalTo(25)
+      }
+    case .topLeft:
+      arrowImageView.image = UIImage(named: "tooltip_arrow_up")
+      arrowImageView.snp.makeConstraints {
+        $0.bottom.equalTo(self.snp.top)
+        $0.trailing.equalToSuperview().inset(40)
+        $0.height.equalTo(13)
+        $0.width.equalTo(25)
+      }
+    case .topMiddle:
+      arrowImageView.image = UIImage(named: "tooltip_arrow_up")
+      arrowImageView.snp.makeConstraints {
+        $0.bottom.equalTo(self.snp.top)
+        $0.trailing.equalToSuperview().inset(40)
+        $0.height.equalTo(13)
+        $0.width.equalTo(25)
+      }
+    case .topRight:
+      arrowImageView.image = UIImage(named: "tooltip_arrow_up")
+      arrowImageView.snp.makeConstraints {
+        $0.bottom.equalTo(self.snp.top)
+        $0.trailing.equalToSuperview().inset(40)
+        $0.height.equalTo(13)
+        $0.width.equalTo(25)
+      }
+    }
     
+    return self
+  }
+  
+  
+  func setSubTitleLabel(with text: NSAttributedString) -> CoachMarkTooltipView {
+    subTitleLabel.attributedText = text
+    return self
+  }
+  
+  func setTitleLabel(with text: NSAttributedString) -> CoachMarkTooltipView {
+    titleLabel.attributedText = text
+    return self
   }
 }
 
@@ -54,20 +134,20 @@ extension CoachMarkTooltipView {
     self.do {
       $0.layer.cornerRadius = 12
       $0.addSubviews(
-        stepLabel,
+        subTitleLabel,
         titleLabel,
         button,
         arrowImageView
       )
     }
     
-    stepLabel.snp.makeConstraints {
+    subTitleLabel.snp.makeConstraints {
       $0.centerX.equalToSuperview()
       $0.top.equalToSuperview().inset(28)
     }
     
     titleLabel.snp.makeConstraints {
-      $0.top.equalTo(stepLabel.snp.bottom).offset(8)
+      $0.top.equalTo(subTitleLabel.snp.bottom).offset(8)
       $0.leading.trailing.equalToSuperview().inset(24)
     }
     
@@ -78,5 +158,3 @@ extension CoachMarkTooltipView {
     }
   }
 }
-
-
