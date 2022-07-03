859c645a5fd8c0c5c3b6fb8393ff901870c8e1dd
woookdev <hjw0911@gmail.com>
2022-07-03 20:09:56 +0900

    feat: HomeCoachMarkState 추가

---
+++ DailyChaCha/DailyChaCha/Sources/Domain/Models/HomeCoachMark/HomeCoachMarkState.swift
@@ -0,0 +1,13 @@
+//
+//  HomeCoachMarkState.swift
+//  DailyChaCha
+//
+//  Created by Jaewook Hwang on 2022/07/03.
+//  Copyright © 2022 DailyChaCha. All rights reserved.
+//
+
+import Foundation
+
+struct HomeCoachMarkState {
+  
+}
