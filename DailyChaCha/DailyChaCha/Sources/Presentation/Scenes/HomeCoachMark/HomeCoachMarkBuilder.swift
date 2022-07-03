cdfc6dad0f728ea759e9453f1b67b6472063b99f
woookdev <hjw0911@gmail.com>
2022-07-03 17:48:09 +0900

    feat: HomeCoachMarkBuilder boilerplate 구현

---
+++ DailyChaCha/DailyChaCha/Sources/Presentation/Scenes/HomeCoachMark/HomeCoachMarkBuilder.swift
@@ -0,0 +1,56 @@
+//
+//  HomeCoachMarkBuilder.swift
+//  DailyChaCha
+//
+//  Created by Jaewook Hwang on 2022/07/03.
+//  Copyright © 2022 DailyChaCha. All rights reserved.
+//
+
+import RIBs
+
+// MARK: - HomeCoachMarkDependency
+
+protocol HomeCoachMarkDependency: Dependency {
+}
+
+// MARK: - HomeCoachMarkComponent
+
+final class HomeCoachMarkComponent: Component<HomeCoachMarkDependency> {
+}
+
+// MARK: - HomeCoachMarkBuildable
+
+protocol HomeCoachMarkBuildable: Buildable {
+  func build(
+    withListener listener: HomeCoachMarkListener
+  ) -> HomeCoachMarkRouting
+}
+
+// MARK: - HomeCoachMarkBuilder
+
+final class HomeCoachMarkBuilder:
+  Builder<HomeCoachMarkDependency>,
+  HomeCoachMarkBuildable
+{
+  
+  // MARK: - Con(De)structor
+  
+  override init(dependency: HomeCoachMarkDependency) {
+    super.init(dependency: dependency)
+  }
+  
+  // MARK: - HomeCoachMarkBuildable
+  
+  func build(
+    withListener listener: HomeCoachMarkListener
+  ) -> HomeCoachMarkRouting {
+    let component = HomeCoachMarkComponent(dependency: dependency)
+    
+    let viewController = HomeCoachMarkViewController()
+    
+    let interactor = HomeCoachMarkInteractor(presenter: viewController, useCase: HomeCoachMarkUseCaseImpl())
+    interactor.listener = listener
+    
+    return HomeCoachMarkRouter(interactor: interactor, viewController: viewController)
+  }
+}
