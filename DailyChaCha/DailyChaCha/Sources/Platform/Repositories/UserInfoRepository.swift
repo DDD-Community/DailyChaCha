19fe08b5262089fd4ed05edd6655c532073b72a5
woookdev <hjw0911@gmail.com>
2022-07-10 18:25:58 +0900

    feat: UserInfoRepository 추가

---
+++ DailyChaCha/DailyChaCha/Sources/Platform/Repositories/UserInfoRepository.swift
@@ -0,0 +1,35 @@
+//
+//  UserInfoRepository.swift
+//  DailyChaCha
+//
+//  Created by Jaewook Hwang on 2022/07/10.
+//  Copyright © 2022 DailyChaCha. All rights reserved.
+//
+
+import Foundation
+
+import Moya
+import RxMoya
+import RxSwift
+
+protocol UserInfoRepository {
+  func requestUserInfo() -> Single<TokenInfo>
+}
+
+final class UserInfoRepositoryImpl: UserInfoRepository {
+  
+  private let provider = MoyaProvider<UserInfoService>()
+  
+  func requestUserInfo() -> Single<TokenInfo> {
+    return provider.rx.request(.fetchUserInfo)
+      .map({ response in
+        return response
+      })
+      .map(TokenInfo.self)
+      .do { token in
+        print(token)
+      } onError: { error in
+        print(error)
+      }
+  }
+}
