f4994ad04314a5072ed30373788b905557983a15
woookdev <hjw0911@gmail.com>
2022-07-11 01:20:11 +0900

    feat: HomeState 추가

---
+++ DailyChaCha/DailyChaCha/Sources/Domain/Models/Home/HomeState.swift
@@ -0,0 +1,13 @@
+//
+//  HomeState.swift
+//  DailyChaCha
+//
+//  Created by Jaewook Hwang on 2022/07/10.
+//  Copyright © 2022 DailyChaCha. All rights reserved.
+//
+
+import Foundation
+
+struct HomeState {
+  
+}
