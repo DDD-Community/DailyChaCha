d96abd2bad7936a5e880625958301dc38afb67fe
woookdev <hjw0911@gmail.com>
2022-07-11 01:20:45 +0900

    feat: MainTabBuilder boilerplate 추가

---
+++ DailyChaCha/DailyChaCha/Sources/Presentation/Scenes/MainTab/MainTabBuilder.swift
@@ -0,0 +1,56 @@
+//
+//  MainTabBuilder.swift
+//  DailyChaCha
+//
+//  Created by Jaewook Hwang on 2022/07/02.
+//  Copyright © 2022 DailyChaCha. All rights reserved.
+//
+
+import RIBs
+
+// MARK: - MainTabDependency
+
+protocol MainTabDependency: Dependency {
+}
+
+// MARK: - MainTabComponent
+
+final class MainTabComponent: Component<MainTabDependency> {
+}
+
+// MARK: - MainTabBuildable
+
+protocol MainTabBuildable: Buildable {
+  func build(
+    withListener listener: MainTabListener
+  ) -> MainTabRouting
+}
+
+// MARK: - MainTabBuilder
+
+final class MainTabBuilder:
+  Builder<MainTabDependency>,
+  MainTabBuildable
+{
+  
+  // MARK: - Con(De)structor
+  
+  override init(dependency: MainTabDependency) {
+    super.init(dependency: dependency)
+  }
+  
+  // MARK: - MainTabBuildable
+  
+  func build(
+    withListener listener: MainTabListener
+  ) -> MainTabRouting {
+    let component = MainTabComponent(dependency: dependency)
+    
+    let viewController = MainTabViewController()
+    
+    let interactor = MainTabInteractor(presenter: viewController, useCase: MainTabUseCaseImpl())
+    interactor.listener = listener
+    
+    return MainTabRouter(interactor: interactor, viewController: viewController)
+  }
+}
