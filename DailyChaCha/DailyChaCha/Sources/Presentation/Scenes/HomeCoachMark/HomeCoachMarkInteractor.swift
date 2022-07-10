22f84098ee656bf2003e0f7aa5a80e7e00bd4ab1
woookdev <hjw0911@gmail.com>
2022-07-10 15:13:54 +0900

    feat: coachMark를 위한 메타 데이터 및 mutation 추가

--- DailyChaCha/DailyChaCha/Sources/Presentation/Scenes/HomeCoachMark/HomeCoachMarkInteractor.swift
+++ DailyChaCha/DailyChaCha/Sources/Presentation/Scenes/HomeCoachMark/HomeCoachMarkInteractor.swift
@@ -34,10 +34,56 @@ final class HomeCoachMarkInteractor:
     case ruleTwo
     case ruleThree
     case brokenTower
+    case brokenCharacter
+    
+    var nextCoachMark: CoachMarkType? {
+      switch self {
+      case .ruleOne:
+        return .ruleTwo
+      case .ruleTwo:
+        return .ruleThree
+      case .ruleThree:
+        return nil
+      case .brokenTower:
+        return nil
+      case .brokenCharacter:
+        return nil
+      }
+    }
+    
+    var subTitle: String? {
+      switch self {
+      case .ruleOne:
+        return "Rule 01"
+      case .ruleTwo:
+        return "Rule 02"
+      case .ruleThree:
+        return "Rule 03"
+      case .brokenTower:
+        return "보상이 망가졌어요!"
+      case .brokenCharacter:
+        return "차근이가 아파요!"
+      }
+    }
+    
+    var title: String? {
+      switch self {
+      case .ruleOne:
+        return "정해진 시간에 운동을 완료하면 오늘의 보상을 받을 수 있어요."
+      case .ruleTwo:
+        return "운동을 하지 않으면 있던 보상이 망가지거나 차근이가 병들어요."
+      case .ruleThree:
+        return "상단에서 루틴을 바꿀 수 있어요."
+      case .brokenTower:
+        return "운동을 2회 놓칠 때마다 아이템이 하나씩 망가져요. 다시 운동하면 원래대로 돌아올 거에요."
+      case .brokenCharacter:
+        return "운동을 1회 놓칠 때마다 차근이가 아파해요. 다시 운동하면 원래대로 돌아올 거에요."
+      }
+    }
   }
   
   enum Mutation {
-    
+    case setCoachMarkType(CoachMarkType)
   }
   
   // MARK: - Properties
@@ -63,16 +109,42 @@ extension HomeCoachMarkInteractor {
   // MARK: - Mutate
   
   func mutate(action: HomeCoachMarkAction) -> Observable<Mutation> {
-    Observable<Mutation>.empty()
+    switch action {
+    case let .setCoachMarkType(type):
+      return setCoachMarkType(type: type)
+      
+    case .tooltipButtonTapped:
+      return tooltipStepMovementMutation()
+    }
   }
   
   // MARK: - Mutations
+  
+  private func setCoachMarkType(type: CoachMarkType) -> Observable<Mutation> {
+    let mutation = Mutation.setCoachMarkType(type)
+    
+    return Observable<Mutation>.just(mutation)
+  }
+  
+  private func tooltipStepMovementMutation() -> Observable<Mutation> {
+    
+    if let nextCoachMark = currentState.coachMarkType?.nextCoachMark {
+      return Observable<Mutation>.just(Mutation.setCoachMarkType(nextCoachMark))
+    } else {
+      return Observable<Mutation>.empty()
+    }
+  }
   
   
   // MARK: - Reduce
   
   func reduce(state: HomeCoachMarkState, mutation: Mutation) -> HomeCoachMarkState {
     var newState = state
+    
+    switch mutation {
+    case let .setCoachMarkType(type):
+      newState.coachMarkType = type
+    }
     
     return newState
   }
