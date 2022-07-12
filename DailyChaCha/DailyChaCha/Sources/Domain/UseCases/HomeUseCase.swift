4c8bbf81e66625b6c0b27c568a0f4e58f4562c7e
woookdev <hjw0911@gmail.com>
2022-07-12 23:52:54 +0900

    feat: HomeUseCase 추가

---
+++ DailyChaCha/DailyChaCha/Sources/Domain/UseCases/HomeUseCase.swift
@@ -0,0 +1,17 @@
+//
+//  HomeUseCase.swift
+//  DailyChaCha
+//
+//  Created by Jaewook Hwang on 2022/07/12.
+//  Copyright © 2022 DailyChaCha. All rights reserved.
+//
+
+import Foundation
+
+protocol HomeUseCase {
+  
+}
+
+class HomeUseCaseImpl: HomeUseCase {
+  
+}
