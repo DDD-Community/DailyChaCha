8a60da962f0c645ab57c65e8d4b4f19b269fe4f1
woookdev <hjw0911@gmail.com>
2022-07-12 23:54:31 +0900

    feat: HomeBuilder boilerplate 구현 추가

---
+++ DailyChaCha/DailyChaCha/Sources/Presentation/Scenes/Home/HomeBuilder.swift
@@ -0,0 +1,56 @@
+//
+//  HomeBuilder.swift
+//  DailyChaCha
+//
+//  Created by Jaewook Hwang on 2022/07/02.
+//  Copyright © 2022 DailyChaCha. All rights reserved.
+//
+
+import RIBs
+
+// MARK: - HomeDependency
+
+protocol HomeDependency: Dependency {
+}
+
+// MARK: - HomeComponent
+
+final class HomeComponent: Component<HomeDependency> {
+}
+
+// MARK: - HomeBuildable
+
+protocol HomeBuildable: Buildable {
+  func build(
+    withListener listener: HomeListener
+  ) -> HomeRouting
+}
+
+// MARK: - HomeBuilder
+
+final class HomeBuilder:
+  Builder<HomeDependency>,
+  HomeBuildable
+{
+  
+  // MARK: - Con(De)structor
+  
+  override init(dependency: HomeDependency) {
+    super.init(dependency: dependency)
+  }
+  
+  // MARK: - HomeBuildable
+  
+  func build(
+    withListener listener: HomeListener
+  ) -> HomeRouting {
+    let component = HomeComponent(dependency: dependency)
+    
+    let viewController = HomeViewController()
+    
+    let interactor = HomeInteractor(presenter: viewController, useCase: HomeUseCaseImpl())
+    interactor.listener = listener
+    
+    return HomeRouter(interactor: interactor, viewController: viewController)
+  }
+}
