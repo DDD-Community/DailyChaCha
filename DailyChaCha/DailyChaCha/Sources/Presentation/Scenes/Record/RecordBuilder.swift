575fe4f950eb4a181349d3f244282fbaa8eb3d25
woookdev <hjw0911@gmail.com>
2022-07-21 22:03:21 +0900

    feat: RecordBuilder boilerplate 추가

--- DailyChaCha/DailyChaCha/Sources/Presentation/Scenes/Record/RecordBuilder.swift
+++ DailyChaCha/DailyChaCha/Sources/Presentation/Scenes/Record/RecordBuilder.swift
@@ -6,4 +6,54 @@
 //  Copyright © 2022 DailyChaCha. All rights reserved.
 //
 
-import Foundation
+import RIBs
+
+// MARK: - RecordDependency
+
+protocol RecordDependency: Dependency {
+}
+
+// MARK: - RecordComponent
+
+final class RecordComponent: Component<RecordDependency> {
+}
+
+// MARK: - RecordBuildable
+
+protocol RecordBuildable: Buildable {
+  func build(
+    withListener listener: RecordListener
+  ) -> RecordRouting
+}
+
+// MARK: - RecordBuilder
+
+final class RecordBuilder:
+  Builder<RecordDependency>,
+  RecordBuildable
+{
+  
+  // MARK: - Con(De)structor
+  
+  override init(dependency: RecordDependency) {
+    super.init(dependency: dependency)
+  }
+  
+  // MARK: - RecordBuildable
+  
+  func build(
+    withListener listener: RecordListener
+  ) -> RecordRouting {
+    let component = RecordComponent(dependency: dependency)
+    
+    let viewController = RecordViewController()
+    
+    let interactor = RecordInteractor(presenter: viewController)
+    interactor.listener = listener
+    
+    return RecordRouter(
+      interactor: interactor,
+      viewController: viewController
+    )
+  }
+}
