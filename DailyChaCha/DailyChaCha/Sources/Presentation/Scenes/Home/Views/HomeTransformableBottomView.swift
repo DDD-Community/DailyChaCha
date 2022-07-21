60e3bd3a579b9176187cc215d55cd9b62bb7c280
woookdev <hjw0911@gmail.com>
2022-07-21 19:53:06 +0900

    feat: HomeTransformableBottomView 레이아웃 구현

--- DailyChaCha/DailyChaCha/Sources/Presentation/Scenes/Home/Views/HomeTransformableBottomView.swift
+++ DailyChaCha/DailyChaCha/Sources/Presentation/Scenes/Home/Views/HomeTransformableBottomView.swift
@@ -8,9 +8,29 @@
 
 import UIKit
 
+import BonMot
+
+enum HomeBottomViewMode {
+  case folded
+  case unfolded
+}
+
 final class HomeTransformableBottomView: UIView {
   
-  private let backgroundView = UIView()
+  lazy var backgroundView = UIView().then {
+    $0.backgroundColor = .white
+    $0.addGestureRecognizer(panGesture)
+    $0.isUserInteractionEnabled = true
+  }
+  
+  private(set) var currentMode: HomeBottomViewMode = .unfolded
+  
+  let panGesture = UIPanGestureRecognizer()
+  
+  private let handleView = UIView().then {
+    $0.backgroundColor = UIColor.black.withAlphaComponent(0.1)
+    $0.layer.cornerRadius = 2
+  }
   
   private let rewardImageView = UIImageView()
   
@@ -18,10 +38,16 @@ final class HomeTransformableBottomView: UIView {
   
   private let subTitleLabel = UILabel()
   
-  private let exerciseButton = UIButton()
+  private let exerciseButton = UIButton().then {
+    $0.setTitle("바로 운동하기", for: .normal)
+    $0.layer.cornerRadius = 9
+    $0.backgroundColor = DailyChaChaAsset.Colors.primary800.color
+    $0.titleLabel?.font = .systemFont(ofSize: 14, weight: .regular)
+  }
   
   override init(frame: CGRect) {
     super.init(frame: frame)
+    setupUI()
   }
   
   required init?(coder: NSCoder) {
@@ -32,8 +58,86 @@ final class HomeTransformableBottomView: UIView {
     super.layoutSubviews()
     
     backgroundView.roundCorners([.topLeft, .topRight],
-      radius: 10
+      radius: 20
     )
+  }
+  
+  func updateLayout(mode: HomeBottomViewMode) {
+    currentMode = mode
+    switch mode {
+    case .folded:
+      let title = "21일 17시간 남음".styled(with: .font(.systemFont(ofSize: 14, weight: .regular)),
+        .color(DailyChaChaAsset.Colors.black.color))
+      
+      subTitleLabel.isHidden = true
+      
+      titleLabel.attributedText = title
+      titleLabel.snp.remakeConstraints {
+        $0.leading.equalTo(rewardImageView.snp.trailing).offset(20)
+        $0.centerY.equalToSuperview()
+      }
+      
+      rewardImageView.snp.updateConstraints {
+        $0.top.equalToSuperview().inset(20)
+        $0.leading.equalToSuperview().inset(20)
+        $0.leading.equalToSuperview().inset(10)
+        $0.size.equalTo(32)
+      }
+      
+      exerciseButton.snp.remakeConstraints {
+        $0.top.equalToSuperview().offset(20)
+        $0.width.equalTo(100)
+        $0.height.equalTo(32)
+        $0.trailing.equalToSuperview().inset(20)
+      }
+      
+      UIView.animate(withDuration: 0.3) {
+        self.layoutIfNeeded()
+      }
+    case .unfolded:
+      let title = "운동 시작까지 21일 7시간".styled(with: .font(.systemFont(ofSize: 18, weight: .medium)))
+      
+      subTitleLabel.isHidden = false
+      
+      titleLabel.attributedText = title
+      
+      titleLabel.snp.remakeConstraints {
+        $0.leading.equalTo(rewardImageView.snp.trailing).offset(20)
+        $0.top.equalTo(handleView.snp.bottom).offset(22)
+      }
+      
+      rewardImageView.snp.updateConstraints {
+        $0.top.equalToSuperview().inset(28)
+        $0.leading.equalToSuperview().inset(24)
+        $0.leading.equalToSuperview().inset(10)
+        $0.size.equalTo(100)
+      }
+      
+      exerciseButton.snp.remakeConstraints {
+        $0.top.equalTo(subTitleLabel.snp.bottom).offset(16)
+        $0.width.equalTo(100)
+        $0.height.equalTo(32)
+        $0.leading.equalTo(rewardImageView.snp.trailing).offset(20)
+      }
+      
+      UIView.animate(withDuration: 0.3) {
+        self.layoutIfNeeded()
+      }
+    }
+  }
+  
+  func configure(with info: NextExerciseInfo) {
+    
+    rewardImageView.image = UIImage(named: "img_default_empty_object")
+    
+    let title = "운동 시작까지 21일 7시간".styled(with: .font(.systemFont(ofSize: 18, weight: .medium)))
+    
+    titleLabel.attributedText = title
+    
+    let subTitle = "연속 100일 째".styled(with: .font(.systemFont(ofSize: 14, weight: .regular)),
+     .color(DailyChaChaAsset.Colors.gray500.color))
+    
+    subTitleLabel.attributedText = subTitle
   }
 }
 
@@ -45,8 +149,47 @@ extension HomeTransformableBottomView {
       $0.backgroundColor = .clear
     }
     
+    backgroundView.addSubviews(
+      handleView,
+      rewardImageView,
+      titleLabel,
+      subTitleLabel,
+      exerciseButton
+    )
+    
     backgroundView.snp.makeConstraints {
       $0.edges.equalToSuperview()
+    }
+    
+    handleView.snp.makeConstraints {
+      $0.centerX.equalToSuperview()
+      $0.top.equalToSuperview().inset(8)
+      $0.width.equalTo(32)
+      $0.height.equalTo(4)
+    }
+    
+    rewardImageView.snp.makeConstraints {
+      $0.top.equalToSuperview().inset(28)
+      $0.leading.equalToSuperview().inset(24)
+      $0.bottom.equalToSuperview().inset(10)
+      $0.size.equalTo(100)
+    }
+    
+    titleLabel.snp.makeConstraints {
+      $0.leading.equalTo(rewardImageView.snp.trailing).offset(20)
+      $0.top.equalTo(handleView.snp.bottom).offset(22)
+    }
+    
+    subTitleLabel.snp.makeConstraints {
+      $0.top.equalTo(titleLabel.snp.bottom).offset(8)
+      $0.leading.equalTo(rewardImageView.snp.trailing).offset(20)
+    }
+    
+    exerciseButton.snp.makeConstraints {
+      $0.top.equalTo(subTitleLabel.snp.bottom).offset(16)
+      $0.leading.equalTo(rewardImageView.snp.trailing).offset(19)
+      $0.width.equalTo(100)
+      $0.height.equalTo(32)
     }
   }
 }
