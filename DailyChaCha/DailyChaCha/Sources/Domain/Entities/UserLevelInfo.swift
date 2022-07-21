284acad341e6777860ea55d81c0e6af29c6d273b
woookdev <hjw0911@gmail.com>
2022-07-21 19:24:41 +0900

    feat: 관련 모델 추가 및 불필요한 모델 제거

---
+++ DailyChaCha/DailyChaCha/Sources/Domain/Entities/UserLevelInfo.swift
@@ -0,0 +1,14 @@
+//
+//  UserLevelInfo.swift
+//  DailyChaCha
+//
+//  Created by Jaewook Hwang on 2022/07/14.
+//  Copyright © 2022 DailyChaCha. All rights reserved.
+//
+
+import Foundation
+
+struct UserLevelInfo: Decodable {
+  let goal: String?
+  let level: Int
+}
